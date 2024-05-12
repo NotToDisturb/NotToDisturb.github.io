488f7822ec9d8fa28ee4b1a573f8c5b346c3e621
Disturbo <nottodisturb.disturbo@gmail.com>
2024-05-12 16:21:27 +0200

    Add toggleable speculative symbols

--- scripts/SoHScript.js
+++ scripts/SoHScript.js
@@ -1,10 +1,11 @@
-const MAX_ASSET_HEIGHT = 300;
+const MAX_ASSET_HEIGHT = 400;
 const ASSET_GAP = 5;
 const CANVAS_WIDTH = 700;
 const CANVAS_HEIGHT = 40;
 
 var config = {};
-var sohAssets = {}
+var sohSymbols = {}
+var sohSpeculativeSymbols = {}
 
 function loaded() {
     getNavbar();
@@ -20,31 +21,43 @@ function getConfig() {
             var configText = "";
             configText = xhr.responseText;
             config = JSON.parse(configText);
-            getSoHAssets();
+            loadSoHSymbols(config.soh_script.letters_to_symbols, sohSymbols);
+            loadSoHSymbols(config.soh_script.letters_to_speculative_symbols, sohSpeculativeSymbols);
         }
     }
     xhr.send();
 }
 
-function getSoHAssets() {
-    var sohChars = Object.keys(config.soh_script.letters);
-    var fistLetter = sohChars[0];
-    sohAssets[fistLetter] = new Image();
-    for (var n = 0; n < sohChars.length; n++) {
-        var currentChar = sohChars[n];
-        if (n + 1 < sohChars.length) {
-            var nextChar = sohChars[n + 1];
-            sohAssets[nextChar] = new Image();
-            sohAssets[currentChar].onload = (function (nextChar) {
-                sohAssets[nextChar].src = config.soh_script.letters[nextChar];
-            })(nextChar);
+function loadSoHSymbols(letterToSymbolMap, loadedSymbols) {
+    var letters = Object.keys(letterToSymbolMap);
+    var fistLetter = letters[0];
+    loadedSymbols[fistLetter] = new Image();
+    for (var n = 0; n < letters.length; n++) {
+        var currentChar = letters[n];
+        if (n + 1 < letters.length) {
+            var nextLetter = letters[n + 1];
+            loadedSymbols[nextLetter] = new Image();
+            loadedSymbols[currentChar].onload = (function (nextChar) {
+                loadedSymbols[nextChar].src = letterToSymbolMap[nextChar];
+            })(nextLetter);
         }
     }
-    sohAssets[fistLetter].src = config.soh_script.letters[fistLetter]
+    loadedSymbols[fistLetter].src = letterToSymbolMap[fistLetter]
+}
+
+function getSoHSymbol(letter, include_speculative) {
+    if (letter in sohSymbols) {
+        return sohSymbols[letter];
+    } else if (include_speculative && letter in sohSpeculativeSymbols) {
+        return sohSpeculativeSymbols[letter];
+    } else {
+        return sohSymbols["unk"];
+    }
 }
 
 function updateEnglishToSoH() {
-    english = document.getElementById("from_english").value;
+    var english = document.getElementById("from_english").value;
+    var include_speculative = document.getElementById("include_speculative").checked;
     var canvas = document.getElementById("result"),
         ctx = canvas.getContext("2d"),
         show_canvas = document.getElementById("show"),
@@ -54,12 +67,12 @@ function updateEnglishToSoH() {
     var cumulativeWidth = ASSET_GAP;
     for(var n = 0; n < english.length; n++) {
         lowercase = english[n].toLowerCase();
-        var sohAsset = lowercase in sohAssets ? sohAssets[lowercase] : sohAssets["unk"];
-        var sizeRatio = sohAsset.naturalWidth / sohAsset.naturalHeight;
-        var heightRatio = sohAsset.naturalHeight / MAX_ASSET_HEIGHT;
+        var sohSymbol = getSoHSymbol(lowercase, include_speculative);
+        var sizeRatio = sohSymbol.naturalWidth / sohSymbol.naturalHeight;
+        var heightRatio = sohSymbol.naturalHeight / MAX_ASSET_HEIGHT;
         var effectiveHeight = CANVAS_HEIGHT * heightRatio;
         var effectiveWidth = effectiveHeight * sizeRatio;
-        ctx.drawImage(sohAsset, cumulativeWidth, (CANVAS_HEIGHT - effectiveHeight) / 2, effectiveWidth, effectiveHeight);
+        ctx.drawImage(sohSymbol, cumulativeWidth, (CANVAS_HEIGHT - effectiveHeight) / 2, effectiveWidth, effectiveHeight);
         cumulativeWidth += effectiveWidth + ASSET_GAP;
     }
     show_ctx.clearRect(0, 0, show_canvas.width, show_canvas.height);
